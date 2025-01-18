64417df51305920b1c1a42e4968fb93bec244cf3
Abdurakhmanov_Daniyel <daniel.kapchagay@gmail.com>
2025-01-19 00:11:06 +0500

    Python Variables

--- lab1.py
+++ lab1.py
@@ -7,7 +7,33 @@ print(sys.version) #PYTHON Get Started
 if 5 > 2: #at least one space is required, 4 is the most commonly used. In the same block - the same number of spaces
     print("Five is greater than two!") #PYTHON Syntax
 
-x = 5 #variables
-y = "Hello, World!" #PYTHON Syntax
+x1 = 5 #variables
+y1 = "Hello, World!" #PYTHON Syntax
 
 """comments may be like this. it will be ignored""" #PYTHON Comments
+
+x2 = 5
+y2 = "John"
+print(x2)
+print(y2)
+
+x3 = 4       # x3 is of type int
+x3 = "Sally" # x3 is now of type str
+print(x3)
+
+x4 = str(3)    # x4 will be '3'
+y4 = int(3)    # y4 will be 3
+z4 = float(3)  # z4 will be 3.0
+
+x5 = 5
+y5 = "John"
+print(type(x5))
+print(type(y5))
+
+x6 = "John"
+# is the same as
+x6 = 'John'
+
+a = 4
+A = "Sally" #this is a constant
+#A will not overwrite a
