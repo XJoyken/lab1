f6c3809d79b24de4de1b5bd61b26838b61950ece
Abdurakhmanov_Daniyel <daniel.kapchagay@gmail.com>
2025-01-18 23:56:07 +0500

    Python Syntax

--- lab1.py
+++ lab1.py
@@ -3,3 +3,9 @@ import sys #PYTHON Get Started
 print("Hello, World!") #PYTHON HOME
 
 print(sys.version) #PYTHON Get Started
+
+if 5 > 2: #at least one space is required, 4 is the most commonly used. In the same block - the same number of spaces
+    print("Five is greater than two!") #PYTHON Syntax
+
+x = 5 #variables
+y = "Hello, World!" #PYTHON Syntax
