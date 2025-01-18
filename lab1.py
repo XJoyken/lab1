2792fc911726ec14dc5e23d293bb52b6be92e073
Abdurakhmanov_Daniyel <daniel.kapchagay@gmail.com>
2025-01-19 02:41:41 +0500

    Format strings

--- lab1.py
+++ lab1.py
@@ -402,3 +402,33 @@ a = "Hello"
 b = "World"
 c = a + " " + b
 print(c)
+
+
+#String Format.
+"""
+We cannot do this:
+age = 36
+txt = "My name is John, I am " + age
+print(txt)
+"""
+#instead of this we can use f-strings or the format() method:
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+#Placeholders and Modifiers. A placeholder can contain variables, operations, functions, and modifiers to format the value:
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+"""
+A placeholder can include a modifier to format the value.
+A modifier is included by adding a colon : followed by a legal formatting type, like .2f which means fixed point number with 2 decimals:
+"""
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+#A placeholder can contain Python code, like math operations:
+txt = f"The price is {20 * 59} dollars"
+print(txt)
